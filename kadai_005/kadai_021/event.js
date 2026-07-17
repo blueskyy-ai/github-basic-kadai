f187a3693a6b1ced27bed292d97d4b810bbc6b11
blueskyy-ai <blue.skyy@softbank.ne.jp>
2026-07-17 10:46:51 +0900

    Delete kadai_005 directory

--- kadai_005/kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-setTimeout(() => {
-    text.textContent = 'クリックしました';
-}, 2000);
-});
